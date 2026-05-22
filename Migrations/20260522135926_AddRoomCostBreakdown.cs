ce990cb01b17c87004fd26adcfe5d1e19bfc67b7
sajid-332 <sajidmahmud000789@gmail.com>
2026-05-22 20:15:42 +0600

    Admin registration

---
+++ Migrations/20260522135926_AddRoomCostBreakdown.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bachelor_s_Point.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoomCostBreakdown : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
